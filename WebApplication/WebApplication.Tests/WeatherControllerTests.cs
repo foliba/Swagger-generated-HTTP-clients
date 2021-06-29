67aaf08c1357c5918cccd9f8ea2a94c0881464df
Roman Alexis Anastasini <roman-alexis.anastasini@zeiss.com>
2021-06-29 22:17:33 +0200

    ✅ add more tests for the weather controller

--- WebApplication/WebApplication.Tests/WeatherControllerTests.cs
+++ WebApplication/WebApplication.Tests/WeatherControllerTests.cs
@@ -21,5 +21,22 @@ namespace WebApplication.Tests
             res.Should().NotBeNull();
             res.Count.Should().BePositive();
         }
+
+        [Fact]
+        public async Task ShouldReturnCorrectAmountOfElements()
+        {
+            var expectedAmount = 10;
+            var res = await _weatherForecastClient.GetAsync(expectedAmount);
+            res.Count.Should().Be(expectedAmount);
+        }
+        
+        [Fact]
+        public async Task ShouldReturnDefaultFiveElements()
+        {
+            var weatherForecastClient = new WeatherForecastClient(BASE_URI, Client);
+            const int expectedAmount = 5;
+            var res = await weatherForecastClient.GetAsync(amount: null);
+            res.Count.Should().Be(expectedAmount);
+        }
     }
 }
